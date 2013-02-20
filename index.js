feac2706bea0d51b624f535290a8aed17ee98240
Reza Akhavan <reza@akhavan.me>
2013-02-19 22:19:12 -0700

    yolo upgrades
    Signed-off-by: Reza Akhavan <reza@akhavan.me>

--- index.js
+++ index.js
@@ -1,52 +1,104 @@
 var game = {
   generation: 0,
   board: [],
-  height: 512,
-  width: 512,
+  preset: {
+    name: 'Criss Cross',
+    image: 'seed-000.png'
+  },
+  height: undefined,
+  width: undefined,
   element: undefined,
   context: undefined,
   keepPlaying: false,
   makeBoard: function() {
     var outcome = [];
-    for (var x = 0 ; x < this.height ; x++) {
+    for (var x = 0 ; x < this.width ; x++) {
       outcome[x] = [];
-      for (var y = 0 ; y < this.width ; y++) {
+      for (var y = 0 ; y < this.height ; y++) {
         outcome[x][y] = false;
       }
     }
     return outcome;
   },
+  setSize: function(height, width) {
+    this.height = height;
+    this.width = width;
+    $("#board").attr("width", width).attr("height", height);
+  },
   initialize: function() {
+    //setup the board
+    this.setSize(512, 512);
     this.board = this.makeBoard();
+    this.positionBoard();
     
+    //get canvas stuff
     this.element = $("#board").get(0);
     this.context = this.element.getContext("2d");
     
+    //bind mouse events
+    this.element.onmousemove = function(e) {
+      event.preventDefault();
+      //find x/y of mouse
+      var rect = game.element.getBoundingClientRect();
+      var x = Math.ceil(e.clientX - rect.left);
+      var y = Math.ceil(e.clientY - rect.top);
+      
+      //update mouse location
+      $('.position-x').text(x);
+      $('.position-y').text(y);
+      
+      //should we draw?
+      if (!game.element.isDrawing) {
+        return;
+      }
+      game.fillPoint(x, y);
+    };
+    this.element.onmousedown = function(e) {
+      event.preventDefault();
+      game.element.isDrawing = true;
+      
+      //find x/y of mouse
+      var rect = game.element.getBoundingClientRect();
+      var x = Math.ceil(e.clientX - rect.left);
+      var y = Math.ceil(e.clientY - rect.top);
+      
+      game.fillPoint(x, y);
+    };
+    this.element.onmouseup = function(e) {
+      event.preventDefault();
+      game.element.isDrawing = false;
+    };
+    this.element.mouseout = function(e) {
+      event.preventDefault();
+    };
+    
+    //load the seed image
     var imageObj = new Image();
     imageObj.onload = function() {
       game.context.drawImage(imageObj, 0, 0);
       
-      var imgData = game.context.getImageData(0, 0, game.height, game.width).data;
-      
-      for (var x = 0 ; x < game.height ; x++) {
-        for (var y = 0 ; y < game.width ; y++) {
+      var imgData = game.context.getImageData(0, 0, game.width, game.height).data;
+      for (var x = 0 ; x < imageObj.width ; x++) {
+        for (var y = 0 ; y < imageObj.height ; y++) {
           var red = imgData[((game.width * y) + x) * 4];
           var green = imgData[((game.width * y) + x) * 4 + 1];
           var blue = imgData[((game.width * y) + x) * 4 + 2];
+          //var alpha = imgData[((game.width * y) + x) * 4 + 3];
           
-          var alive = (red == 255 && green == 255 && blue == 255);
-          game.board[x][y] = !alive;
+          var alive = (red == 0 && green == 0 && blue == 0);
+          game.board[x][y] = alive;
         }
       }
       
-      game.drawBoard;
+      game.drawBoard();
     };
-    imageObj.src = 'lena.png';
+    imageObj.src = this.preset.image;
+    $('.preset-selected').text(this.preset.name);
   },
   drawBoard: function() {
-    var newImage = this.context.getImageData(0, 0, this.height, this.width);
-    for (var x = 0 ; x < this.board.length ; x++) {
-      for (var y = 0 ; y < this.board[x].length ; y++) {
+    var newImage = this.context.getImageData(0, 0, this.width, this.height);
+    for (var x = 0 ; x < this.width ; x++) {
+      for (var y = 0 ; y < this.height ; y++) {
           newImage.data[((this.width * y) + x) * 4]     = this.board[x][y] ? 0 : 255; //red
           newImage.data[((this.width * y) + x) * 4 + 1] = this.board[x][y] ? 0 : 255; //green
           newImage.data[((this.width * y) + x) * 4 + 2] = this.board[x][y] ? 0 : 255; //blue
@@ -56,6 +108,10 @@ var game = {
     this.context.putImageData(newImage, 0, 0);
     
     $(".generation").text(this.generation);
+  },
+  fillPoint: function(x, y) {
+    game.context.fillRect(x, y, 1, 1);
+    game.board[x][y] = true;
   },
   nextGeneration: function() {
     var keepItGoing = game.keepPlaying;
@@ -97,20 +153,12 @@ var game = {
     if (game.keepPlaying) setTimeout("game.nextGeneration()", 1);
   },
   get: function(x, y) {
-    //x = x % this.width;
-    //y = y % this.height;
-    
     if (x < 0) x = this.width - 1;
     if (x >= this.width) x = 0;
     if (y < 0) y = this.height - 1;
     if (y >= this.height) y = 0;
     
     return this.board[x][y];
-  },
-  set: function(x, y, z) {
-    x = x % this.width;
-    y = y % this.height;
-    this.board[x][y] = v;
   },
   neighbors: function(x, y) {
     var count = 0;
@@ -123,28 +171,62 @@ var game = {
     if (this.get(x - 1, y + 1)) { count++; }
     if (this.get(x, y + 1)) { count++; }
     return count;
+  },
+  positionBoard: function() {
+    console.log($(window).height());
+    $('.right').css({
+      height: $(window).height()
+    });
+    $('.board-container').css({
+      position:'relative',
+      left: ($('.right').width() - $('.board-container').outerWidth())/2,
+      top: ($('.right').height() - $('.board-container').outerHeight())/2
+    });
+  },
+  controls: {
+    restart: function(e) {
+      game.keepPlaying = false;
+      game.generation = 0;
+      game.initialize();
+    },
+    next: function(e) {
+      game.nextGeneration();
+    },
+    play: function(e) {
+      game.keepPlaying = true;
+      game.nextGeneration();
+    },
+    stop: function(e) {
+      game.keepPlaying = false;
+    },
+    clear: function(e) {
+      game.keepPlaying = false;
+      game.generation = 0;
+      game.board = game.makeBoard();
+      game.drawBoard();
+    }
   }
 };
 
 
 //controls
-$('.ctrl-restart').click(function(e) {
-  game.keepPlaying = false;
-  game.generation = 0;
+$('.btn-restart').click(game.controls.restart);
+$('.btn-next').click(game.controls.next);
+$('.btn-play').click(game.controls.play);
+$('.btn-stop').click(game.controls.stop);
+$('.btn-trash').click(game.controls.clear);
+
+//preset conrols
+$('[data-preset]').click(function(e) {
+  game.controls.stop();
+  game.preset.name = $(this).data('name');
+  game.preset.image = $(this).data('preset');
   game.initialize();
-  game.drawBoard();
-});
-$('.ctrl-next').click(function(e) {
-  game.nextGeneration();
-});
-$('.ctrl-play').click(function(e) {
-  game.keepPlaying = true;
-  game.nextGeneration();
-});
-$('.ctrl-stop').click(function(e) {
-  game.keepPlaying = false;
 });
 
 //start the game of life
 game.initialize();
 
+$(window).resize(function(){
+  game.positionBoard();
+});
